0128044fd3651db5a07856bdb5b7254e4e1f111f
Nam Anh Tran <steve200994@gmail.com>
2025-12-03 15:16:16 +1100

    a

--- frontend/cakeshop/src/app/homepage/homepage.ts
+++ frontend/cakeshop/src/app/homepage/homepage.ts
@@ -1,9 +1,10 @@
 import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import AOS from 'aos';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
